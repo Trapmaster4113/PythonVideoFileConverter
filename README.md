e7fd7c74db9656f2deb5452aaa313ae3354183b8
Tyler <tcao2560@gmail.com>
2026-03-11 11:25:35 -0400

    Updated tktest.py to use an object container rather than several global variables. Also updated readme to include instructions for making changes for their own application

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Select what file type to convert to,
 Select your input type
 Select your output folder
 and convert
+
+Use "python -m PyInstaller --onefile --windowed tktest.py" after saving the changes to tktest.py
+This will update the .exe file with your changes
--- tktest.py
+++ tktest.py
@@ -2,12 +2,18 @@ import tkinter as tk
 from tkinter import filedialog
 from moviepy import VideoFileClip
 import os
+class GlobalVariables():
+    def __init__(self):
+        self.inputFile = None
+        self.inputFolder = None
+        self.outputFolder = None
+        self.code = None
+        self.file = None
 #Function used to change label text for Error Message on the bottom right and the Input/Output labels in the top right
 #Takes in 2 parameters: 
 #name: used to see which label to change
 #text: this is the new text
 def changeText(name, text):
-
     if name == "input":
         inputLabel.config(text=text)
     elif name == "output":
@@ -17,85 +23,75 @@ def changeText(name, text):
 #input(): function used to get location of file/folder to be changed
 def input():
     #Makes sure the function uses global variables instead of local variables with the same name
-    global inputFile
-    global inputFolder
     #If User selects option 1, will ask for an input file rather than an input folder
     if w.get() == 1:
         #Creates a variable storing the file type and ensures correct file type
-        inputFile = filedialog.askopenfilename(
+        f.inputFile = filedialog.askopenfilename(
         title = "Select a file",
         filetypes=[("Video Files", "*.mp4 *.mkv *.mov *.ogv *.webm *.avi")]
         )
         #Changes the input label to display the file name
-        changeText("input", inputFile.title())
+        changeText("input", f.inputFile.title())
         #Ensures that the inputFolder is null just in case don't know if necessary
-        inputFolder = None
+        f.inputFolder = None
     #If User selects option 2, will ask for an input folder rather than an input file
     elif w.get() == 2:
         #Creates a variable storing the folder location
-        inputFolder = filedialog.askdirectory(title="Select a folder")
+        f.inputFolder = filedialog.askdirectory(title="Select a folder")
         #Changes the input label to display the folder name
-        changeText("input", inputFolder.title())
+        changeText("input", f.inputFolder.title())
         #Ensures that the inputFile is null just in case don't know if necessary
-        inputFile = None
+        f.inputFile = None
 #enterOutputFolder(): function used to get location of folder to place the converted video
 def enterOutputFolder():
     #Makes sure the function uses global variables instead of local variables with the same name
-    global outputFolder
-    global outputLabel
     #Creates a variable storing the folder location
-    outputFolder = filedialog.askdirectory(title="Select a folder")
+    f.outputFolder = filedialog.askdirectory(title="Select a folder")
     #Changes the output label to display the folder name
-    changeText("output", outputFolder.title())
+    changeText("output", f.outputFolder.title())
 #fileType(): function used to set variables used later in the conversion process
 def fileType():
     #Makes sure the function uses global variables instead of local variables with the same name
-    global code
-    global file
     #Over complicated if statements
     if v.get() == 4:
-        code = "libtheora"
-        file = ".ogv"
+        f.code = "libtheora"
+        f.file = ".ogv"
     elif v.get() == 5:
-        code = "libvpx"
-        file = ".webm"
+        f.code = "libvpx"
+        f.file = ".webm"
     elif v.get() == 6:
-        code = ""
-        file = ".avi"
+        f.code = ""
+        f.file = ".avi"
     #For some reason these 3 file types use the same code
     else:
-        code = "libx264"
+        f.code = "libx264"
         if v.get() == 1:
-            file = ".mp4"
+            f.file = ".mp4"
         elif v.get() == 2:
-            file = ".mkv"
+            f.file = ".mkv"
         elif v.get() == 3:
-            file = ".mov"
+            f.file = ".mov"
 #convert(): function used to covert the file or the entire folder of videos into the desired type while skipping files that were already converted
 def convert():
     #Makes sure the function uses global variables instead of local variables with the same name
-    global outputFolder
-    global inputFile
-    global inputFolder
-    global clipDuration
     #If no outputFolder selected, throw error and stops function
-    if outputFolder is None:
+    if f.outputFolder is None:
         changeText("error","No Output Selected")
         return
     #Checks if user wants to change a single file
-    if not inputFile is None:
+    if not f.inputFile is None:
         #Removes the absolute location and gets only the file name
-        name = inputFile.split("/")[-1]
+        name = f.inputFile.split("/")[-1]
         #Removes the file type from the end
         name = name[:name.find(".")]
         #Creates a VideoFileClip variable with the video file that can be manipulated
-        clip = VideoFileClip(inputFile)
+        clip = VideoFileClip(f.inputFile)
         #Shortens the video by a small percentage to remove any dead frames (For some reason, all clips have them at the end)
         clip = clip.subclipped(0, clip.duration * clipDuration)
         #Gets file path of output folder to check if going to make a duplicate
-        contents2 = os.listdir(outputFolder)
+        contents2 = os.listdir(f.outputFolder)
         #Checks if duplicate exits
-        if name+file in contents2:
+        if name+f.file in contents2:
             #Throws error due to duplicate file
             changeText("error", "Duplicate File Located")
             #Reset input text and file
@@ -104,31 +100,31 @@ def convert():
             #Exits function
             return
         #Creates the new video YAY!
-        clip.write_videofile(outputFolder+"/"+name+file, codec=code)
+        clip.write_videofile(f.outputFolder+"/"+name+f.file, codec=f.code)
         #Reset input text and file
-        inputFile = None
+        f.inputFile = None
         changeText("input", "")
     #Checks if user wants to change all files in a folder
-    elif not inputFolder is None:
+    elif not f.inputFolder is None:
         #Gets folder paths for both input and output folders
-        contents = os.listdir(inputFolder)
-        contents2 = os.listdir(outputFolder)
+        contents = os.listdir(f.inputFolder)
+        contents2 = os.listdir(f.outputFolder)
         #Iterates through all files in the inputfolder
         for i in contents:
             #Removes file type from all file names
             name = i[:i.find(".")]
             #Checks if duplicate exits in outputFolder
-            if name+file in contents2:
+            if name+f.file in contents2:
                 continue
 
             #Creates a VideoFileClip variable with the video file that can be manipulated
-            clip = VideoFileClip(inputFile)
+            clip = VideoFileClip(f.inputFile)
             #Shortens the video by a small percentage to remove any dead frames (For some reason, all clips have them at the end)
             clip = clip.subclipped(0, clip.duration * clipDuration)
             #Creates the new video YAY!
-            clip.write_videofile(outputFolder+"/"+name+file, codec=code)
+            clip.write_videofile(f.outputFolder+"/"+name+f.file, codec=f.code)
         #Reset input text and file
-        inputFolder = None
+        f.inputFolder = None
         changeText("input", "")
     else:
         #If no input selected, throws error and stops function
@@ -140,13 +136,9 @@ def convert():
 #Used for creating desktop application with Tkinter
 root = tk.Tk()
 #Creating global variables so functions can share (don't know if necessary)
-inputFile = None
-inputFolder = None
-outputFolder = None
+f = GlobalVariables()
 #Technically an arbitray number, but I think it works
 clipDuration = 0.983
-code = None
-file = None
 #creating the text labels for input,output, and error
 #.grid is used to position them on the application
 inputLabel = tk.Label(root, text="")
